19b3e529d2f44800bf7579bea3430c95207455e3
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-10-26 18:43:35 -0500

    ADD new component spinner loading

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div class="lds-circle">
+      <div></div>
+    </div>
+  );
+};
+
+export default Spinner;
